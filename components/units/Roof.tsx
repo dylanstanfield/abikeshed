afdb744c98f998b5d6790d95882b50c56900304f
Dylan Stanfield <dylan@dstanfield.com>
2021-01-02 17:28:06 -0500

    dont't animate roof

--- components/units/Roof.tsx
+++ components/units/Roof.tsx
@@ -1,10 +1,7 @@
-import { useState, useRef } from 'react'
 import { Cylinder } from '@react-three/drei'
-import { useFrame } from 'react-three-fiber'
 
 import { Board } from '../parts/Board'
 import { colors } from '../colors'
-import { Group } from 'three'
 
 interface OffsetProps {
   offset: number;
@@ -23,38 +20,17 @@ const Nail: React.FC<OffsetProps> = ({ offset }) => (
   </Cylinder>
 )
 
-export const Roof = () => {
-  const roof = useRef<Group>()
-  const [open, setOpen] = useState(false)
-  const toggleRoof = () => setOpen(!open)
-
-  useFrame(() => {
-    if (open && roof.current.rotation.x > -0.75) {
-      roof.current.rotation.x -= 0.01133333
-    }
-    if (open && roof.current.position.y < 5) {
-      roof.current.position.y += 0.02222222
-    }
-    if (!open && roof.current.rotation.x < 0.27) {
-      roof.current.rotation.x += 0.01133333
-    }
-    if (!open && roof.current.position.y > 3) {
-      roof.current.position.y -= 0.02222222
-    }
-  })
-  
-  return (
-    <group ref={ roof } rotation={ [0.27, 0, 0] } position={ [0, 3, 0.03] } onClick={toggleRoof}>
-      <Board scale={ [6, 0.1, 5] } position={ [0, 0, 0] } color={ colors.frame } />
-      <group position={ [-2.8, 0, 0] } onClick={toggleRoof}>
-        { [...Array(20).keys()].map((i) => <Strip offset={ i * 0.3 } key={i} />)}
-      </group>
-      <group position={ [-2.72, 0, 1.85] } onClick={toggleRoof}>
-        { [...Array(7).keys()].map((i) => <Nail offset={ i * 0.9 } key={i} />)}
-      </group>
-      <group position={ [-2.72, 0, -1.85] } onClick={toggleRoof}>
-        { [...Array(7).keys()].map((i) => <Nail offset={ i * 0.9 } key={i} />)}
-      </group>
+export const Roof = () => (
+  <group rotation={ [0.27, 0, 0] } position={ [0, 3, 0.03] }>
+    <Board scale={ [6, 0.1, 5] } position={ [0, 0, 0] } color={ colors.frame } />
+    <group position={ [-2.8, 0, 0] }>
+      { [...Array(20).keys()].map((i) => <Strip offset={ i * 0.3 } key={i} />)}
     </group>
-  )
-}
+    <group position={ [-2.72, 0, 1.85] }>
+      { [...Array(7).keys()].map((i) => <Nail offset={ i * 0.9 } key={i} />)}
+    </group>
+    <group position={ [-2.72, 0, -1.85] }>
+      { [...Array(7).keys()].map((i) => <Nail offset={ i * 0.9 } key={i} />)}
+    </group>
+  </group>
+)
